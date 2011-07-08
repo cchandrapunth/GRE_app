2f9d115e245d1932d88eadb1d3d98d302f3f3db2
cchandrapunth <cchandrapunth@gmail.com>
2011-07-08 16:30:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GREApp::Application.config.secret_token = '98f3f4d1bc7e064159e94e3000ec958f9238e115fd2f971698d015e7ce52200795ad0c5fbe88f225c5e18f93c9f676c12bfc3183bdf086bfa501d7895079fa6d'
